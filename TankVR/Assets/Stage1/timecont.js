6cfdf35b1888010498fb01f0b12961ad8b5f074c
KMN_note <teraumareninja@gmail.com>
2016-11-27 12:25:07 +0900

    shino 発表用 がんばれ

--- TankVR/Assets/Stage1/timecont.js
+++ TankVR/Assets/Stage1/timecont.js
@@ -14,10 +14,10 @@ GUI.Label(Rect(0,50,Screen.width,100),"Time:"+time,MyStyle);
 
 function timecont(){
  time--;
- if(time == 60 && Score.scr >= 10 ){
+ if(time == 0 && Score.scr >= 10 ){
  Application.LoadLevel("clear_St1");
  }
-  if(time == 60 && Score.scr < 10 ){
+  if(time == 0 && Score.scr < 10 ){
  Application.LoadLevel("GameOver");
  }
 }
